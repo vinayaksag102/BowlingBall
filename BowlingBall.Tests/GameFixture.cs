154fa9c1118d9e317ba0bb6a21701ab8d56ebb1e
unknown <vinayaksag102@gmail.com>
2020-12-05 22:16:16 +0530

    first commit

---
+++ BowlingBall.Tests/GameFixture.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace BowlingBall.Tests
+{
+    [TestClass]
+    public class GameFixture
+    {
+        private Game game;
+
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            game = new Game();
+
+        }
+
+        [TestMethod]
+        public void Gutter_game_score_should_be_zero_test()
+        {
+            RollMany(0, 20);
+            Assert.AreEqual(0, game.GetScore());
+        }
+
+       
+        [TestMethod]
+        public void CanRollAllOnes_test()
+        {
+            RollMany(1, 20);
+            Assert.AreEqual(20, game.GetScore());
+        }
+
+        [TestMethod]
+        public void CanRollSpare_test()
+        {
+            game.Roll(5);
+            game.Roll(5);
+            game.Roll(3);
+            RollMany(0, 17);
+            Assert.AreEqual(16, game.GetScore());
+        }
+
+        [TestMethod]
+        public void CanRollStrike_test()
+        {
+            game.Roll(10);
+            game.Roll(3);
+            game.Roll(4);
+            RollMany(0, 16);
+            Assert.AreEqual(24, game.GetScore());
+        }
+
+        [TestMethod]
+        public void CanRollPerfectGame_test()
+        {
+
+            // No. of pins knocked in each  Roll
+            List<int> RollList = new List<int>() { 10, 9, 1, 5, 5, 7, 2, 10, 10, 10, 9, 0, 8, 2, 9, 1, 10 };
+
+            foreach (var roll in RollList)
+                game.Roll(roll);
+
+            Assert.AreEqual(187, game.GetScore());
+        }
+
+        private void RollMany(int pins, int rolls)
+        {
+            for (int i = 0; i < rolls; i++)
+                game.Roll(pins);
+        }
+
+        
+    }
+}
---
+++ BowlingBall/Game.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingBall
+{
+    public class Game
+    {
+        private int[] rolls = new int[21];
+        int currentRoll = 0;
+
+
+        public int GetScore()
+        {
+            // Returns the final score of the game.
+            var score = 0;
+            var rollIndex = 0;
+            for (var frame = 0; frame < 10; frame++)
+            {
+                //Strike
+                if (IsStrike(rollIndex))
+                {
+                    score += GetStrikeScore(rollIndex);
+                    rollIndex++;
+                }
+                //Spare
+                else if (IsSpare(rollIndex))
+                {
+                    score += GetSpareScore(rollIndex);
+                    rollIndex += 2;
+                }
+                else
+                {
+                    score += GetStandardScore(rollIndex);
+                    rollIndex += 2;
+                }
+            }
+            return score;
+        }
+
+        public void Roll(int pins)
+        {
+           rolls[currentRoll++] = pins;
+        }
+
+
+        private int GetStrikeScore(int rollIndex)
+        {
+            return rolls[rollIndex] + rolls[rollIndex + 1] + rolls[rollIndex + 2];
+        }       
+
+        private int GetStandardScore(int rollIndex)
+        {
+            return rolls[rollIndex] + rolls[rollIndex + 1];
+        }
+
+        private int GetSpareScore(int rollIndex)
+        {
+            return rolls[rollIndex] + rolls[rollIndex + 1] + rolls[rollIndex + 2];
+        }
+
+        private bool IsSpare(int rollIndex)
+        {
+            return rolls[rollIndex] + rolls[rollIndex + 1] == 10;
+        }
+
+        private bool IsStrike(int rollIndex)
+        {
+            return rolls[rollIndex] == 10;
+        }
+    }
+}
